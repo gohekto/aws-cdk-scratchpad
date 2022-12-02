67d727e797a540b6e8579c9a766fcbd475b12545
Sebastian Korfmann <sebastian@korfmann.net>
2022-12-02 16:20:06 +0100

    Cleanup tests

--- patterns/token-vending-machine-untrusted-code/__test__/index.test.ts
+++ patterns/token-vending-machine-untrusted-code/__test__/index.test.ts
@@ -2,22 +2,15 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 import { putObject } from '../../test-helper/helper';
 import { TokenVendingMachineUntrustedCode } from "..";
 import { cdkSpec, createTestApp } from "@hekto/cloud-spec-aws-cdk";
-import { CfnOutput } from "aws-cdk-lib";
 
 const testApp = createTestApp({
-  creator: (stack) => {
+  creator: (stack, outputs) => {
     const component = new TokenVendingMachineUntrustedCode(stack, 'token-vending-machine-untrusted-code');
 
-    new CfnOutput(stack, "bucket", {
-      value: component.bucket.bucketName,
-    })
-
-    new CfnOutput(stack, "table", {
-      value: component.inventory.tableName,
-    })
-
-    new CfnOutput(stack, "dispatcher", {
-      value: component.dispatcher.functionArn,
+    outputs({
+      "bucket": component.bucket.bucketName,
+      "table": component.inventory.tableName,
+      "dispatcher": component.dispatcher.functionName,
     })
   },
 });
@@ -28,7 +21,7 @@ describe("token vending machine", () => {
   });
 
   cdkSpec.it("works if user is scoped with matching tenant", async (outputs) => {
-    const { bucket, dispatcher, table } = outputs[testApp.stackName]
+    const { bucket, dispatcher } = outputs
 
     await putObject(bucket, 'skorfmann/foo.txt', 'Hello, world!', 'text/plain')
 
@@ -48,7 +41,6 @@ describe("token vending machine", () => {
     const payload = JSON.parse(Buffer.from(lambda.Payload!.buffer).toString());
     expect(payload.lambda.statusCode).toEqual(200);
     const targetPayload = JSON.parse(Buffer.from(payload.lambda.payload, 'base64').toString("ascii"));
-    console.log({lambda, payload, targetPayload})
     expect(targetPayload).toBeNull();
 
   }, 10_000);
